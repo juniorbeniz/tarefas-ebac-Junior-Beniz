f1456fb23e52ebcc691a005d292f3b8122285055
juniorbeniz <walmirj0@gmail.com>
2023-06-27 22:43:10 -0300

    Primeiro commit

---
+++ mod5/PrimeiroProjeto/PrimeiraClasse/src/br/com/jbeniz/PrimeiraClasse.java
@@ -0,0 +1,21 @@
+package br.com.jbeniz;
+
+public class PrimeiraClasse {
+
+	public static void main(String [] args) {
+		// TODO Auto-generated method stub
+		String teste = args[0];
+		
+		if (teste.equalsIgnoreCase("dev")) {
+			System.out.println("Executando teste com DEV");
+		} else {
+			if (teste.equalsIgnoreCase("Junior")) {
+				System.out.println("Serei um Especialista Back-End Java");
+			} else {
+				System.out.println("Hello World!!!");
+			}
+			
+		}
+	}
+
+}  
