f4e43cfb065366ed05c00b596f5196b7a1d0133e
juniorbeniz <walmirj0@gmail.com>
2023-09-11 21:45:03 -0300

    envio de tarefa do mod19 sobre Reflections

---
+++ mod19/TarefaReflections/src/reflections/Tab.java
@@ -0,0 +1,10 @@
+package reflections;
+
+
+public class Tab {
+
+    @Tabela(nome = "Tabela 1")
+    public void sayName() {
+    }
+
+}
---
+++ mod19/TarefaReflections/src/reflections/Tabela.java
@@ -0,0 +1,14 @@
+package reflections;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface Tabela {
+    String nome();
+
+    String autor() default "Junior Beniz";
+}
